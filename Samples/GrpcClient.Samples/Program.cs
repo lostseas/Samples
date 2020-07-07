a1a8222fe4ad07643b90dd2bd9a6cde188c91b29
lostsea <lostsea@DESKTOP-SKNQ1VF>
2020-07-07 15:55:36 +0800

    add grpc demo

---
+++ Samples/GrpcClient.Samples/Program.cs
@@ -0,0 +1,30 @@
+﻿using Grpc.Net.Client;
+using GrpcService.Samples;
+using System;
+using System.Threading.Tasks;
+
+namespace GrpcClient.Samples
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "lostsea" });
+            Console.WriteLine("Greeter 服务返回数据: " + reply.Message);
+
+            var client1 = new Cat.CatClient(channel);
+            var reply1 = client1.GetCatName(
+                new CatRequest { Name = "妮妮" });
+            Console.WriteLine("Cat 服务返回数据: " + reply1.Message);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Samples/GrpcService.Samples/Services/CatService.cs
@@ -0,0 +1,27 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcService.Samples.Services
+{
+    public class CatService : Cat.CatBase
+    {
+        private readonly ILogger<CatService> _logger;
+        public CatService(ILogger<CatService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<CatReply> GetCatName(CatRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new CatReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
+
